d36e3e0f11d5ba7d9cd34255dca03822b6ac3a5e
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-07-06 15:16:00 +0545

    final commit for now. will make changes after i learn more about react.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className="container footercontent">
+                <p>Just a simple corona details app I made while learning React.</p><br/>
                 <p>Used disease.sh API</p><br/>
                 <p>Naresh Ban</p>
             </div>
